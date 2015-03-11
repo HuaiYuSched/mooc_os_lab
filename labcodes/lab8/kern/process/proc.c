85f28e3ed6d260ee4386d67327d429846a6b8931
Youren SHen <shenyouren@gmail.com>
2015-03-11 09:22:45 +0800

    LAB 8 Practice 2

--- labcodes/lab8/kern/process/proc.c
+++ labcodes/lab8/kern/process/proc.c
@@ -145,6 +145,7 @@ alloc_proc(void) {
 //		proc->lab6_run_pool = NULL;
 		proc->lab6_stride = 0;
 		proc->lab6_priority = 0;
+		proc->filesp = NULL;
     }
     return proc;
 }
@@ -496,6 +497,8 @@ do_fork(uint32_t clone_flags, uintptr_t stack, struct trapframe *tf) {
     if((copy_mm(clone_flags,proc))!=0)
 		goto bad_fork_cleanup_kstack;
 	copy_thread(proc,stack,tf);
+	if((copy_files(clone_flags,proc))!=0)
+		goto bad_fork_cleanup_fs;		//Add for LAB 8
 	
     local_intr_save(intr_flag);
     {
@@ -618,6 +621,157 @@ load_icode(int fd, int argc, char **kargv) {
      * (7) setup trapframe for user environment
      * (8) if up steps failed, you should cleanup the env.
      */
+
+	int ret = -E_NO_MEM;
+	if(current->mm!= NULL)
+	{
+		panic("load icode: current mm must be empty.\n");
+	}
+	struct mm_struct *mm;
+	// (1) create a new mm for current process
+	if((mm = mm_create()) == NULL)
+		goto bad_mm;
+    // (2) create a new PDT, and mm->pgdir= kernel virtual addr of PDT
+	if(setup_pgdir(mm) != 0)
+		goto bad_pgdir_cleanup_mm;
+
+	struct elfhdr elf;
+    //    (3.1) read raw data content in file and resolve elfhdr
+	if((ret = load_icode_read(fd,&elf,sizeof(struct elfhdr),0))!=0)
+		goto bad_pgdir_cleanup_mm;
+	//    Is this program valid?
+	if(elf.e_magic != ELF_MAGIC)
+	{
+		ret = -E_INVAL_ELF;
+		goto bad_elf_cleanup_pgdir;
+	}
+	// Find TXT/DATA section
+	uint32_t phnum;
+	struct proghdr __ph, *ph=&__ph;
+	uint32_t vm_flags = 0, perm = PTE_U;
+	for(phnum = 0; phnum < elf.e_phnum ; phnum++)
+	{
+    //    (3.2) read raw data content in file and resolve proghdr based on info in elfhdr
+		if((ret = load_icode_read(fd, ph, sizeof(struct proghdr), elf.e_phoff+(sizeof(__ph)*phnum)))!=0)
+			goto out;
+		if(ph->p_type != ELF_PT_LOAD)
+			continue;
+		if(ph->p_filesz > ph->p_memsz)
+		{
+			ret = -E_INVAL_ELF;
+			goto bad_cleanup_mmap;
+		}
+		if(ph->p_filesz == 0)
+			continue;
+		if(ph->p_flags & ELF_PF_X)	vm_flags |= VM_EXEC;
+		if (ph->p_flags & ELF_PF_W) vm_flags |= VM_WRITE;
+		if (ph->p_flags & ELF_PF_R) vm_flags |= VM_READ;
+		if (vm_flags & VM_WRITE) perm |= PTE_W; 
+    //    (3.3) call mm_map to build vma related to TEXT/DATA
+		if((ret = mm_map(mm, ph->p_va, ph->p_memsz,vm_flags,NULL))!=0)
+			goto bad_cleanup_mmap;
+	//	if((ret = load_icode_read(fd, buf, ph->p_filesz, ph->p_offset))==0)
+	//		goto bad_cleanup_mmap;
+		uint32_t  offset =  ph->p_offset;
+		size_t size, off;
+		uintptr_t start = ph->p_va, end, la=ROUNDDOWN(start,PGSIZE);
+		end = ph->p_va + ph->p_filesz;
+
+		struct Page *page;
+     //   (3.4) callpgdir_alloc_page to allocate page for TEXT/DATA, read contents in file
+     //         and copy them into the new allocated pages
+		while(start < end)
+		{
+			if((page=pgdir_alloc_page(mm->pgdir,la,perm))==NULL)
+			{
+				ret = -E_NO_MEM;
+				goto bad_cleanup_mmap;
+			}
+			off = start - la, size = PGSIZE - off, la += PGSIZE;
+			if( end < la)
+				size -= la - end;
+			if((ret = load_icode_read(fd, page2kva(page)+off, size, offset))!=0)
+				goto bad_cleanup_mmap;
+			start +=size, offset += size;
+		}
+     //   (3.5) callpgdir_alloc_page to allocate pages for BSS, memset zero in these pages
+		end = ph->p_va + ph->p_memsz;
+		if( start < la)
+		{
+			if(start == end)
+				continue;
+			off = start + PGSIZE - la, size = PGSIZE - off;
+			if(end < la)
+				size -= la - end;
+			memset(page2kva(page) + off, 0, size);
+			start += size;
+			assert((end < la && start == end) || (end >= la && start == la));
+		}
+		while (start < end)
+		{
+			if((page = pgdir_alloc_page(mm->pgdir, la, perm))==NULL)
+				goto bad_cleanup_mmap;
+			off = start -la, size = PGSIZE - off, la +=PGSIZE;
+			if(end <la)
+				size -= la-end;
+			memset(page2kva(page) + off, 0, size);
+			start += size;
+		}
+	}
+	sysfile_close(fd);
+    // (4) call mm_map to setup user stack, and put parameters into user stack
+	vm_flags = VM_READ|VM_WRITE|VM_STACK;
+	if((ret = mm_map(mm, USTACKTOP - USTACKSIZE, USTACKSIZE, vm_flags, NULL))!=0)
+		goto bad_cleanup_mmap;
+	assert(pgdir_alloc_page(mm->pgdir, USTACKTOP-PGSIZE , PTE_USER) != NULL);
+	assert(pgdir_alloc_page(mm->pgdir, USTACKTOP-2*PGSIZE , PTE_USER) != NULL);
+	assert(pgdir_alloc_page(mm->pgdir, USTACKTOP-3*PGSIZE , PTE_USER) != NULL);
+	assert(pgdir_alloc_page(mm->pgdir, USTACKTOP-4*PGSIZE , PTE_USER) != NULL);
+
+    // (5) setup current process's mm, cr3, reset pgidr (using lcr3 MARCO)
+	mm_count_inc(mm);
+	current->mm=mm;
+	current->cr3 = PADDR(mm->pgdir);
+	lcr3(PADDR(mm->pgdir));
+    // (6) setup uargc and uargv in user stacks
+	uint32_t argv_size = 0,i;
+	for(i=0; i< argc;i++)
+	{
+		argv_size += strnlen(kargv[i], EXEC_MAX_ARG_LEN + 1) +1;
+	}
+	uintptr_t stacktop = USTACKTOP - (argv_size/sizeof(long)+1)*sizeof(long);
+	char **uargv = (char **)(stacktop - argc * sizeof(char*));
+	argv_size = 0;
+	for(i=0; i< argc;i++)
+	{
+
+		uargv[i] = strcpy((char *)(stacktop+argv_size),kargv[i]);
+		argv_size += strnlen(kargv[i], EXEC_MAX_ARG_LEN + 1) +1;
+	}
+	stacktop = (uintptr_t)uargv - sizeof(int);
+	*(int *)stacktop = argc;
+	
+    // (7) setup trapframe for user environment
+	struct trapframe *tf = current->tf;
+	memset(tf, 0 ,sizeof(struct trapframe));
+	tf->tf_cs = USER_CS;
+	tf->tf_ds=tf->tf_es=tf->tf_ss=USER_DS;
+	tf->tf_esp=stacktop;
+	tf->tf_eip=elf.e_entry;
+	tf->tf_eflags = FL_IF;
+	ret = 0;
+
+    // (8) if up steps failed, you should cleanup the env.
+out:
+    return ret;
+bad_cleanup_mmap:
+    exit_mmap(mm);
+bad_elf_cleanup_pgdir:
+    put_pgdir(mm);
+bad_pgdir_cleanup_mm:
+    mm_destroy(mm);
+bad_mm:
+    goto out;
 }
 
 // this function isn't very correct in LAB8
@@ -866,7 +1020,7 @@ init_main(void *arg) {
         panic("create user_main failed.\n");
     }
  extern void check_sync(void);
-    check_sync();                // check philosopher sync problem
+//    check_sync();                // check philosopher sync problem
 
     while (do_wait(0, NULL) == 0) {
         schedule();
