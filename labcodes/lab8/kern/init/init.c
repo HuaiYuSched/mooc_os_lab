75f4b796b46e045d711bb492681fa6e4f8722d4c
Youren SHen <shenyouren@gmail.com>
2015-03-29 17:19:45 +0800

    Add SMP initialization

--- labcodes/lab8/kern/init/init.c
+++ labcodes/lab8/kern/init/init.c
@@ -14,10 +14,16 @@
 #include <proc.h>
 #include <fs.h>
 #include <kmonitor.h>
+#include <mp.h>
+#include <lapic.h>
+#include <sched.h>
+#include <mmu.h>
 
 int kern_init(void) __attribute__((noreturn));
 void grade_backtrace(void);
 static void lab1_switch_test(void);
+static void mpmain(void) __attribute__((noreturn));
+static void startothers(void);
 
 int
 kern_init(void) {
@@ -29,10 +35,12 @@ kern_init(void) {
     const char *message = "(THU.CST) os is loading ...";
     cprintf("%s\n\n", message);
 
-    print_kerninfo();
+//    print_kerninfo();
 
-    grade_backtrace();
-
+//    grade_backtrace();
+	mpinit();
+	lapicinit();
+	cprintf("\ncpu%d: starting xv6\n\n", cpu->id);
     pmm_init();                 // init physical memory management
 
     pic_init();                 // init interrupt controller
@@ -47,13 +55,15 @@ kern_init(void) {
     fs_init();                  // init fs
     
     clock_init();               // init clock interrupt
+	startothers();
     intr_enable();              // enable irq interrupt
 
     //LAB1: CAHLLENGE 1 If you try to do it, uncomment lab1_switch_test()
     // user/kernel mode switch test
     //lab1_switch_test();
+	mpmain();
     
-    cpu_idle();                 // run idle process
+ //   cpu_idle();                 // run idle process
 }
 
 void __attribute__((noinline))
@@ -115,3 +125,103 @@ lab1_switch_test(void) {
     lab1_print_cur_status();
 }
 
+static inline void
+lgdt_cpu(struct segdesc *p, int size)
+{
+volatile ushort pd[3];
+
+  pd[0] = size-1;
+  pd[1] = (uint)p;
+  pd[2] = (uint)p >> 16; 
+  
+  asm volatile("lgdt (%0)" : : "r" (pd));
+}
+
+static void load_gdt()
+{
+  struct cpu *c; 
+  c = &cpus[cpunum()];
+  
+  c->gdt[SEG_KTEXT] = SEG(STA_X | STA_R, 0x0, 0xFFFFFFFF, DPL_KERNEL),
+  c->gdt[SEG_KDATA] = SEG(STA_W, 0x0, 0xFFFFFFFF, DPL_KERNEL),
+  c->gdt[SEG_UTEXT] = SEG(STA_X | STA_R, 0x0, 0xFFFFFFFF, DPL_USER),
+  c->gdt[SEG_UDATA] = SEG(STA_W, 0x0, 0xFFFFFFFF, DPL_USER),
+//  c->gdt[SEG_TSS] = SEGTSS(STS_T32A, (uintptr_t)&ts, sizeof(ts), DPL_KERNEL);
+  c->gdt[SEG_KCPU] = SEG(STA_W, ((uint32_t)&c->cpu), 8, DPL_KERNEL);
+  lgdt_cpu(c->gdt,sizeof(c->gdt));
+  loadgs(SEG_KCPU << 3);
+
+  cpu = c;
+  proc = 0;
+}
+
+// Other CPUs jump here from entryother.S.
+static void
+mpenter(void)
+{
+//  switchkvm(); 
+  enable_paging();
+  lapicinit();
+  mpmain();
+}
+
+// Common CPU setup code.
+static void
+mpmain(void)
+{
+  load_gdt();	//build a new GDT for each cpu
+  cprintf("cpu%d: starting\n", cpu->id);
+//  loadidt();       // load idt register
+  xchg(&cpu->started, 1); // tell startothers() we're up
+  //schedule();     // start running processes
+  cpu_idle();
+}
+
+pde_t entrypgdir[];  // For entry.S
+
+// Start the non-boot (AP) processors.
+static void
+startothers(void)
+{
+  extern uchar _binary_entryother_start[], _binary_entryother_size[];
+  uchar *code;
+  struct cpu *c;
+  char *stack;
+
+  // Write entry code to unused memory at 0x7000.
+  // The linker has placed the image of entryother.S in
+  // _binary_entryother_start.
+  code = p2v(0x7000);
+  memmove(code, _binary_entryother_start, (uint32_t)_binary_entryother_size);
+
+  for(c = cpus; c < cpus+ncpu; c++){
+    if(c == cpus+cpunum())  // We've started already.
+      continue;
+
+    // Tell entryother.S what stack to use, where to enter, and what 
+    // pgdir to use. We cannot use kpgdir yet, because the AP processor
+    // is running in low  memory, so we use entrypgdir for the APs too.
+    stack = alloc_page();
+    *(void**)(code-4) = stack + KSTACKSIZE;
+    *(void**)(code-8) = mpenter;
+    *(int**)(code-12) = (void *) v2p(entrypgdir);
+
+    lapicstartap(c->id, v2p(code));
+
+    // wait for cpu to finish mpmain()
+    while(c->started == 0)
+      ;
+  }
+}
+
+// Boot page table used in entry.S and entryother.S.
+// Page directories (and page tables), must start on a page boundary,
+// hence the "__aligned__" attribute.  
+// Use PTE_PS in page directory entry to enable 4Mbyte pages.
+__attribute__((__aligned__(PGSIZE)))
+pde_t entrypgdir[NPDEENTRY] = {
+  // Map VA's [0, 4MB) to PA's [0, 4MB)
+  [0] = (0) | PTE_P | PTE_W | PTE_PS,
+  // Map VA's [KERNBASE, KERNBASE+4MB) to PA's [0, 4MB)
+  [KERNBASE>>PDXSHIFT] = (0) | PTE_P | PTE_W | PTE_PS,
+};
