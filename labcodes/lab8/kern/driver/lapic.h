f7a2d8611100d21085402842ad0f5d9a86057ea1
Youren SHen <shenyouren@gmail.com>
2015-03-29 17:12:00 +0800

    add apic driver

---
+++ labcodes/lab8/kern/driver/lapic.h
@@ -0,0 +1,8 @@
+// lapic.c
+int             cpunum(void);
+extern volatile uint*    lapic;
+void            lapiceoi(void);
+void            lapicinit(void);
+void            lapicstartap(uchar, uint);
+void            microdelay(int);
+
